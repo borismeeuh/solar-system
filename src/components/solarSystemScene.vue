7e5d7004532a58337d4eda5a81b35159cba3c597
Boris Kamstra <boriskamstra34@gmail.com>
2024-07-10 17:05:52 +0200

    Canvas ingesteld

---
+++ src/components/solarSystemScene.vue
@@ -0,0 +1,26 @@
+<template>
+    <canvas id="canvas" class="canvas" ref="canvas" />
+</template>
+<script lang="ts" setup>
+// @ts-ignore
+import { createScene } from '../scenes/SolarSystem';
+import { ref, watch } from 'vue';
+
+const canvas = ref<HTMLCanvasElement | null>(null);
+
+watch(canvas, () => {
+    if (canvas.value) {
+        createScene(canvas.value);
+    }
+});
+</script>
+<style>
+body {
+    overflow: hidden;
+}
+
+.canvas {
+    width: 100vw;
+    height: 100vh;
+}
+</style>
