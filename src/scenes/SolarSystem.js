b570ecd85bfd1bac08232d5e5fbf0c59c00dc54d
Boris Kamstra <boriskamstra34@gmail.com>
2024-07-11 17:22:06 +0200

    Zon laten gloeien

--- src/scenes/SolarSystem.js
+++ src/scenes/SolarSystem.js
@@ -1,40 +1,42 @@
-import {
-    Engine,
-    Scene,
-    FreeCamera,
-    Vector3,
-    MeshBuilder,
-    StandardMaterial,
-    Color3,
-    HemisphericLight,
-    ArcRotateCamera,
-    UniversalCamera,
-    Tools
-} from '@babylonjs/core';
+import * as BABYLON from '@babylonjs/core';
 
 const createScene = (canvas) => {
-    const engine = new Engine(canvas);
-    const scene = new Scene(engine);
-    const camera = new ArcRotateCamera(
+    const engine = new BABYLON.Engine(canvas);
+    const scene = new BABYLON.Scene(engine);
+
+    const camera = new BABYLON.ArcRotateCamera(
         'camera1',
-        Tools.ToRadians(45),
-        Tools.ToRadians(45),
+        BABYLON.Tools.ToRadians(45),
+        BABYLON.Tools.ToRadians(45),
         20,
-        new Vector3(0, 0, 0),
+        new BABYLON.Vector3(0, 0, 0),
         scene
     );
-    const sun = MeshBuilder.CreateSphere('sun', { diameter: 10 }, scene);
-
-    camera.setTarget(sun.position);
     camera.attachControl(canvas.value, true);
 
-    const material = new StandardMaterial('box-material', scene);
+    const sun = BABYLON.MeshBuilder.CreateSphere(
+        'sun',
+        { diameter: 10 },
+        scene
+    );
 
-    new HemisphericLight('light', Vector3.Up(), scene);
+    const sunMaterial = new BABYLON.StandardMaterial('sunMaterial', scene);
+    sunMaterial.emissiveColor = new BABYLON.Color3(1, .5, 0)
+    sunMaterial.diffuseTexture = new BABYLON.Texture(
+        'src/textures/sun.jpg',
+        scene
+    );
+    sun.material = sunMaterial;
 
-    material.diffuseColor = Color3.Blue();
-    sun.material = material;
+    let sunlight = new BABYLON.GlowLayer("sunlight", scene);
+    sunlight.intensity = 0.4;
 
+    camera.setTarget(sun.position);
+
+
+
+
+    
     engine.runRenderLoop(() => {
         scene.render();
     });
