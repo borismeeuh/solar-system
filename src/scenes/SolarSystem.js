43682eddf068420ed700efaf58bbd238199f914f
Boris Kamstra <boriskamstra34@gmail.com>
2024-07-15 14:00:18 +0200

    Mercurius ingesteld

--- src/scenes/SolarSystem.js
+++ src/scenes/SolarSystem.js
@@ -3,32 +3,21 @@ import * as BABYLON from '@babylonjs/core';
 const createScene = (canvas) => {
     const engine = new BABYLON.Engine(canvas);
     const scene = new BABYLON.Scene(engine);
-
-    const sunLight = new BABYLON.DirectionalLight("SunLight", new BABYLON.Vector3(1, 0, 0), scene);
-
-    makeCamera()
-    createSun()
-
-    const mercury = BABYLON.MeshBuilder.CreateSphere(
-        'mercury',
-        { diameter: 0.38 },
+    const sunLight = new BABYLON.DirectionalLight(
+        'SunLight',
+        new BABYLON.Vector3(1, 0, 0),
         scene
     );
-    mercury.position = new BABYLON.Vector3(0, 0, 0);
 
-    const mercuryMaterial = new BABYLON.StandardMaterial(
-        'mercuryMaterial',
-        scene
-    );
-    mercuryMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
-    mercuryMaterial.diffuseTexture = new BABYLON.Texture(
-        'src/textures/mercury.jpg',
-        scene
-    );
-    mercury.material = mercuryMaterial;
+    let camera;
+
+    makeCamera();
+    createSun();
+    createMercury();
 
     engine.runRenderLoop(() => {
         scene.render();
+        console.log(camera.position);
     });
 
     window.addEventListener('resize', () => {
@@ -36,23 +25,24 @@ const createScene = (canvas) => {
     });
 
     function makeCamera() {
-        const camera = new BABYLON.ArcRotateCamera(
+        camera = new BABYLON.ArcRotateCamera(
             'camera1',
-            BABYLON.Tools.ToRadians(0),
-            BABYLON.Tools.ToRadians(0),
+            BABYLON.Tools.ToRadians(90),
+            BABYLON.Tools.ToRadians(90),
             20,
             new BABYLON.Vector3(0, 0, 0),
             scene
         );
         camera.attachControl(canvas.value, true);
-        camera.radius = 250;
-        camera.wheelPrecision = 1;
+        camera.radius = 25;
+        camera.wheelPrecision = 5;
+        camera.position = new BABYLON.Vector3(-23.76, 0.06, 8.06);
     }
 
     function createSun() {
         const sun = BABYLON.MeshBuilder.CreateSphere(
             'sun',
-            { diameter: 109 },
+            { diameter: 10.9 },
             scene
         );
 
@@ -67,6 +57,26 @@ const createScene = (canvas) => {
 
         let sunlight = new BABYLON.GlowLayer('sunlight', scene);
         sunlight.intensity = 0.4;
+    }
+
+    function createMercury() {
+        const mercury = BABYLON.MeshBuilder.CreateSphere(
+            'mercury',
+            { diameter: 3.8 },
+            scene
+        );
+        mercury.position = new BABYLON.Vector3(454.1, 0, 0);
+        const mercuryMaterial = new BABYLON.StandardMaterial(
+            'mercuryMaterial',
+            scene
+        );
+        mercuryMaterial.diffuseTexture = new BABYLON.Texture(
+            'src/textures/mercury.jpg',
+            scene
+        );
+        mercuryMaterial.emissiveColor = new BABYLON.Color3(0.1, 0.1, 0.1);
+        mercuryMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+        mercury.material = mercuryMaterial;
     }
 };
 
